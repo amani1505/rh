781c6c8a74c734646cf468f1295d07767bc62bf2
amani1505 <amanideveloper1505@gmail.com>
2024-07-19 13:52:23 +0300

    adding the routes and core folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//   {
+//     path: '',
+//     loadChildren: () =>
+//       import('./modules/layout/layout.module').then((m) => m.LayoutModule),
+//   },
+//   {
+//     path: 'auth',
+//     loadChildren: () =>
+//       import('./modules/auth/auth.module').then((m) => m.AuthModule),
+//   },
+//   { path: '**', redirectTo: 'error/404' },
+];
