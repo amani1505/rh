4a40e8195e2de4f82bb599c2bcc55ec98efa06bb
amani1505 <amanideveloper1505@gmail.com>
2024-08-18 14:16:05 +0300

    Adding color themes for the project

--- src/app/modules/landing/components/navbar/navbar.component.ts
+++ src/app/modules/landing/components/navbar/navbar.component.ts
@@ -1,22 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ThemeService } from '@service/theme/theme.service';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [AngularSvgIconModule, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: '../../landing.component.scss'
+  styleUrl: '../../landing.component.scss',
 })
 export class NavbarComponent {
   isMobileMenuActive: boolean = false;
-  isMobileCategoryActive:boolean=false
-  isOverlayActive:boolean =false
-  menu:boolean = false
-  showMenu:boolean=true
+  isMobileCategoryActive: boolean = false;
+  isOverlayActive: boolean = false;
+  menu: boolean = false;
+  showMenu: boolean = true;
 
+  constructor(public themeService: ThemeService) {}
   categories = [
     {
       title: 'Categories',
@@ -29,18 +30,21 @@ export class NavbarComponent {
 
   toggleMobileMenu() {
     this.isMobileMenuActive = !this.isMobileMenuActive;
-    this.isOverlayActive = !this.isOverlayActive
+    this.isOverlayActive = !this.isOverlayActive;
   }
 
-  toggleCategoryMenu(){
-    this.isMobileCategoryActive = !this.isMobileCategoryActive
-    this.isOverlayActive = !this.isOverlayActive
+  toggleCategoryMenu() {
+    this.isMobileCategoryActive = !this.isMobileCategoryActive;
+    this.isOverlayActive = !this.isOverlayActive;
   }
 
   toggleAccordion(category: any) {
     category.isOpen = !category.isOpen;
   }
- open(){
-  this.menu = !this.menu
- }
+  open() {
+    this.menu = !this.menu;
+  }
+  toggleTheme() {
+    this.themeService.theme = !this.themeService.isDark ? 'dark' : 'light';
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  prefix: '',
-  mode: 'jit',
+  prefix: "",
+  mode: "jit",
   important: false,
-  content: ['./src/**/*.{html,ts}'],
-  darkMode: 'class', // or 'media' or 'class'
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       screens: {
@@ -27,47 +27,47 @@ module.exports = {
       },
       keyframes: {
         wiggle: {
-          '0%, 100%': { transform: 'rotate(-3deg)' },
-          '50%': { transform: 'rotate(3deg)' },
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
         },
-        'fade-in-down': {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(-10px)',
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
           },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)',
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
           },
         },
-        'fade-out-down': {
+        "fade-out-down": {
           from: {
-            opacity: '1',
-            transform: 'translateY(0px)',
+            opacity: "1",
+            transform: "translateY(0px)",
           },
           to: {
-            opacity: '0',
-            transform: 'translateY(10px)',
+            opacity: "0",
+            transform: "translateY(10px)",
           },
         },
-        'fade-in-up': {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(10px)',
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
           },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)',
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
           },
         },
-        'fade-out-up': {
+        "fade-out-up": {
           from: {
-            opacity: '1',
-            transform: 'translateY(0px)',
+            opacity: "1",
+            transform: "translateY(0px)",
           },
           to: {
-            opacity: '0',
-            transform: 'translateY(10px)',
+            opacity: "0",
+            transform: "translateY(10px)",
           },
         },
         slideDown: {
@@ -88,11 +88,11 @@ module.exports = {
         },
       },
       animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
-        'fade-in-down': 'fade-in-down 0.3s ease-out',
-        'fade-out-down': 'fade-out-down 0.3s ease-out',
-        'fade-in-up': 'fade-in-up 0.3s ease-out',
-        'fade-out-up': 'fade-out-up 0.3s ease-out',
+        wiggle: "wiggle 1s ease-in-out infinite",
+        "fade-in-down": "fade-in-down 0.3s ease-out",
+        "fade-out-down": "fade-out-down 0.3s ease-out",
+        "fade-in-up": "fade-in-up 0.3s ease-out",
+        "fade-out-up": "fade-out-up 0.3s ease-out",
         slideDown: "slideDown 1s ease-in-out",
         slideUp: "slideUp 1s ease-in-out",
         slideLeft: "slideLeft 0.5s ease-in-out",
@@ -111,49 +111,49 @@ module.exports = {
         1000: "1000ms",
       },
       boxShadow: {
-        custom: '0px 0px 50px 0px rgb(82 63 105 / 15%)',
+        custom: "0px 0px 50px 0px rgb(82 63 105 / 15%)",
       },
       colors: {
         primary: colors.pink,
-        secondary:colors.green,
+        secondary: colors.green,
         night: {
-          50: '#e4e4eb',
-          100: '#bbbace',
-          200: '#8f8ead',
-          300: '#66658c',
-          400: '#4b4777',
-          500: '#302a62',
-          600: '#2b245b',
-          700: '#241c51',
-          800: '#1c1445',
-          900: '#130030',
+          50: "rgba(229, 231, 235, 1)",
+          100: "rgba(209, 213, 219, 1)",
+          200: "rgba(156, 163, 175, 1)",
+          300: "rgba(107, 114, 128, 1)",
+          400: "rgba(75, 85, 99, 1)",
+          500: "rgba(55, 65, 81, 1)",
+          600: "rgba(44, 55, 75, 1)",
+          700: "rgba(31, 41, 55, 1)",
+          800: "rgba(17, 24, 39, 1)",
+          900: "rgba(10, 13, 20, 1)",
         },
       },
     },
     fontFamily: {
-      poppins: ['Poppins', 'system-ui', 'sans-serif'],
-      nunito: ['Nunito Sans', 'sans-serif'],
+      poppins: ["Poppins", "system-ui", "sans-serif"],
+      nunito: ["Nunito Sans", "sans-serif"],
       raleway: ["Raleway"],
       varela: ["Varela Round", "sans-serif"],
     },
     container: {
       padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
       },
     },
   },
   variants: {
     extend: {},
-    scrollbar: ['dark', 'rounded']
+    scrollbar: ["dark", "rounded"],
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-    require('tailwind-scrollbar')
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("tailwind-scrollbar"),
   ],
-}
+};
