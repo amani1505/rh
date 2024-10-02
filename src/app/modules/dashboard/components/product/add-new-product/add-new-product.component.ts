89f679a942fddce65f1f514f583ed6d959b70604
amani1505 <amanideveloper1505@gmail.com>
2024-10-02 20:44:22 +0300

    Adding some create product feature

--- src/app/modules/dashboard/components/product/add-new-product/add-new-product.component.ts
+++ src/app/modules/dashboard/components/product/add-new-product/add-new-product.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { ImageUploaderComponent } from '@components/image-uploader/image-uploader.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { NormalSelectComponent } from '../../../../../components/form/select/normal-select/normal-select.component';
 
 @Component({
   selector: 'app-add-new-product',
   standalone: true,
-  imports: [ImageUploaderComponent,AngularSvgIconModule],
+  imports: [
+    ImageUploaderComponent,
+    AngularSvgIconModule,
+    NormalSelectComponent,
+  ],
   templateUrl: './add-new-product.component.html',
-  styleUrl: './add-new-product.component.scss'
+  styleUrl: './add-new-product.component.scss',
 })
-export class AddNewProductComponent {
-
-}
+export class AddNewProductComponent {}
